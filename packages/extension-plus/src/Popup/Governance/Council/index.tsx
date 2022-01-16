2af593f4e22aff587ad1e762352fa1f2163af0e0
Nick-1979 <ekbatanifard@gmail.com>
2022-01-16 14:25:31 +0330

    add council

---
+++ packages/extension-plus/src/Popup/Governance/Council/index.tsx
@@ -0,0 +1,116 @@
+// Copyright 2019-2022 @polkadot/extension-plus authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+/* eslint-disable header/header */
+
+import { OpenInNew as OpenInNewIcon, BatchPrediction as BatchPredictionIcon, CheckCircleOutline as CheckCircleOutlineIcon, HowToVote as HowToVoteIcon, RemoveCircleOutline as RemoveCircleOutlineIcon, ThumbDownAlt as ThumbDownAltIcon, ThumbUpAlt as ThumbUpAltIcon, WhereToVote as WhereToVoteIcon } from '@mui/icons-material';
+import { Button, Container, Divider, Grid, Link, LinearProgress, Modal, Paper, Tab, Tabs } from '@mui/material';
+import React, { Dispatch, SetStateAction, useCallback, useEffect, useState } from 'react';
+import ReactDom from 'react-dom';
+
+import useTranslation from '../../../../../extension-ui/src/hooks/useTranslation';
+import getChainInfo from '../../../util/getChainInfo';
+import getCouncil from '../../../util/getCouncil';
+import Councils from './Councils';
+import PlusHeader from '../../common/PlusHeader';
+import Progress from '../../common/Progress';
+import { DeriveReferendumExt, DeriveProposal } from '@polkadot/api-derive/types';
+import getCurrentBlockNumber from '../../../util/getCurrentBlockNumber';
+
+interface Props {
+  chainName: string;
+  showCouncilModal: boolean;
+  setCouncilModalOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function Council({ chainName, setCouncilModalOpen, showCouncilModal }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const [tabValue, setTabValue] = useState('council');
+  const [council, setReferenduns] = useState<DeriveReferendumExt[]>();
+  const [motions, setMotions] = useState<DeriveProposal[]>();
+  const [decimals, setDecimals] = useState<number>(1);
+  const [coin, setCoin] = useState<string>();
+  const [currentBlockNumber, setCurrentBlockNumber] = useState<number>();
+
+  useEffect(() => {
+    // eslint-disable-next-line no-void
+    void getChainInfo(chainName).then((r) => {
+      setDecimals(r.decimals);
+      setCoin(r.coin);
+    });
+
+    // eslint-disable-next-line no-void
+    void getCouncil(chainName, 'council').then(r => {
+      setReferenduns(r);
+    });
+
+
+    getCurrentBlockNumber(chainName).then((n)=>{
+      setCurrentBlockNumber(n);
+    })
+    
+  }, [chainName])
+  const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
+    setTabValue(newValue);
+  };
+
+  const handleCouncilModalClose = useCallback(
+    (): void => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      setCouncilModalOpen(false);
+    },
+    [setCouncilModalOpen]
+  );
+
+
+  return ReactDom.createPortal(
+    <Modal
+      disablePortal
+      // eslint-disable-next-line react/jsx-no-bind
+      onClose={(_event, reason) => {
+        if (reason !== 'backdropClick') {
+          handleCouncilModalClose();
+        }
+      }}
+      open={showCouncilModal}
+    >
+      <div style={{
+        backgroundColor: '#FFFFFF',
+        display: 'flex',
+        height: '100%',
+        maxWidth: 700,
+        position: 'relative',
+        top: '5px',
+        transform: `translateX(${(window.innerWidth - 560) / 2}px)`,
+        width: '560px'
+      }}
+      >
+        <Container disableGutters maxWidth='md'>
+          <PlusHeader action={handleCouncilModalClose} chain={chainName} closeText={'Close'} icon={<HowToVoteIcon />} title={'Council'} />
+          <Grid container>
+            <Grid item xs={12} sx={{ margin: '0px 30px' }}>
+              <Tabs indicatorColor='secondary' onChange={handleTabChange} textColor='secondary' value={tabValue} variant='fullWidth'>
+                <Tab icon={<WhereToVoteIcon fontSize='small' />} iconPosition='start' label='Council' sx={{ fontSize: 11 }} value='council' />
+                <Tab icon={<BatchPredictionIcon fontSize='small' />} iconPosition='start' label='Motions' sx={{ fontSize: 11 }} value='motions' />
+              </Tabs>
+            </Grid>
+            {tabValue === 'council'
+              ? <>{council
+                ? <Council council={council} chainName={chainName} coin={coin} decimals={decimals} currentBlockNumber={currentBlockNumber}/>
+                : <Progress title={'Getting Council ...'} />}
+              </>
+              : ''}
+
+            {tabValue === 'motions'
+              ? <>{motions
+                ? <Council council={council} chainName={chainName} coin={coin} decimals={decimals} />
+                : <Progress title={'Getting Motions ...'} />}
+              </>
+              : ''}
+
+          </Grid>
+        </Container>
+      </div>
+    </Modal>
+    , document.getElementById('root')
+  );
+}
---
+++ packages/extension-plus/src/util/getCouncil.ts
@@ -0,0 +1,74 @@
+// Copyright 2019-2022 @polkadot/extension-plus authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+/* eslint-disable header/header */
+
+import getChainInfo from './getChainInfo';
+import { hexToBn, hexToString } from '@polkadot/util';
+
+const DEFAULT_IDENTITY = {
+  // 'judgements': [],
+  //  'deposit':202580000000,
+  info: {
+    // 'additional':[],
+    display: null,
+    legal: null,
+    web: null,
+    //  'riot':{'none':null},
+    email: null,
+    //  'pgpFingerprint':null,
+    //  'image':{'none':null},
+    twitter: null
+  }
+};
+
+async function getIdentities (_chainName, _address) {
+  console.log(`getting identities of .... on ${_chainName}`);
+
+  const { api } = await getChainInfo(_chainName);
+
+  const identities = await api.query.identity.identityOf.multi(_address);
+
+  const ids = identities.map((id) => {
+    if (!id.toString()) {
+      return DEFAULT_IDENTITY;
+    }
+
+    const jId = JSON.parse(id.toString());
+    // console.log('jId:', jId)
+
+    const info = {
+      // 'judgements': [],
+      //  'deposit':202580000000,
+      info: {
+        // 'additional':[],
+        display: jId?.info?.display?.raw && hexToString(jId?.info?.display?.raw),
+        legal: jId?.info?.legal?.raw && hexToString(jId?.info?.legal?.raw),
+        web: jId?.info?.web?.raw && hexToString(jId?.info?.web?.raw),
+        //  'riot':{'none':null},
+        email: jId?.info?.email?.raw && hexToString(jId?.info?.email?.raw),
+        //  'pgpFingerprint':null,
+        //  'image':{'none':null},
+        twitter: jId?.info?.twitter?.raw && hexToString(jId?.info?.twitter?.raw)
+      }
+    };
+
+    return info;
+  });
+
+  console.log(ids)
+  return ids;
+}
+
+
+export default async function getCouncil(_chain: string, type: string): Promise<any> {
+
+  const { api } = await getChainInfo(_chain);
+
+  
+   const council = await api.derive.council.members();
+   getIdentities(_chain,council)
+
+  console.log('council:', council.toString())
+
+  return council;
+}
