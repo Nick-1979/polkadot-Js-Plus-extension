db0dd95d00b4f92d5a59de4e9a860efb6a2b35ff
Nick-1979 <ekbatanifard@gmail.com>
2022-04-24 02:26:52 +0430

    register service worker

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -5,4 +5,14 @@ import '@polkadot/extension-inject/crossenv';
 
 import { createView, Popup } from '@polkadot/extension-ui';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/background.js')
+    .then(function (registration) {
+      console.log('Registration successful, scope:', registration.scope);
+    })
+    .catch(function (error) {
+      console.log('Service worker registration failed, error:', error);
+    });
+}
+
 createView(Popup);
