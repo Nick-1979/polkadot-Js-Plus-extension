63b91d33dfbb2f2abc67e57546da1e70ee144ae8
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-07-11 12:51:53 +0430

    The user's account address will be highlighted among other pool members account address

--- packages/extension-plus/src/Popup/Staking/Pool/PoolMoreInfo.tsx
+++ packages/extension-plus/src/Popup/Staking/Pool/PoolMoreInfo.tsx
@@ -9,6 +9,7 @@
  * */
 
 import type { AccountId32 } from '@polkadot/types/interfaces';
+import type { PalletNominationPoolsPoolMember } from '@polkadot/types/lookup';
 import type { MembersMapEntry, MyPoolInfo } from '../../../util/plusTypes';
 
 import { BubbleChart as BubbleChartIcon } from '@mui/icons-material';
@@ -25,7 +26,6 @@ import useTranslation from '../../../../../extension-ui/src/hooks/useTranslation
 import { PlusHeader, Popup, ShortAddress } from '../../../components';
 import { SELECTED_COLOR } from '../../../util/constants';
 import Pool from './Pool';
-import type { PalletNominationPoolsPoolMember } from '@polkadot/types/lookup';
 
 interface Props {
   chain: Chain;
@@ -34,10 +34,11 @@ interface Props {
   handleMorePoolInfoClose: () => void;
   pool: MyPoolInfo | undefined;
   poolId: BN;
-  poolsMembers: MembersMapEntry[] | undefined
+  poolsMembers: MembersMapEntry[] | undefined;
+  stakerId: string;
 }
 
-export default function PoolMoreInfo({ api, chain, handleMorePoolInfoClose, pool, poolId, poolsMembers, showPoolInfo }: Props): React.ReactElement<Props> {
+export default function PoolMoreInfo({ api, chain, handleMorePoolInfoClose, pool, poolId, poolsMembers, stakerId, showPoolInfo }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
 
   const myPoolMembers = poolsMembers && pool ? poolsMembers[Number(poolId)] as unknown as MembersMapEntry : undefined;
@@ -56,7 +57,7 @@ export default function PoolMoreInfo({ api, chain, handleMorePoolInfoClose, pool
             const points = api.createType('Balance', member.points); // FIXME: it is points not balance!!
 
             return (
-              <Paper elevation={2} key={index} sx={{ bgcolor: roleIds.includes(accountId) ? SELECTED_COLOR : '', my: 1, p: '1px' }}>
+              <Paper elevation={2} key={index} sx={{ bgcolor: (accountId === stakerId) ? SELECTED_COLOR : '', my: 1, p: '1px' }}>
                 <Grid alignItems='center' container item justifyContent='space-between' sx={{ fontSize: 12 }}>
                   <Grid item xs={1}>
                     <Identicon
